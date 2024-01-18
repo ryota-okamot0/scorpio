525e35a757f974eeb5f48fef780b58b59f05abff
ryota.okamoto <r.okamoto@ridecore.co.jp>
2024-01-18 11:28:26 +0900

    first commit

---
+++ src/main/java/com/example/scorpio/ScorpioApplication.java
@@ -0,0 +1,20 @@
+package com.example.scorpio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+public class ScorpioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScorpioApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, Spring Boot 3!!!";
+    }
+}
